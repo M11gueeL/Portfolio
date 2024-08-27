ee6e60366ada91e8e049fd590d9fa288c8a39150
M11gueeL <themiguemonasterio@gmail.com>
2024-08-26 22:47:50 -0400

    finally

--- src/Projects/ProjectsData.js
+++ src/Projects/ProjectsData.js
@@ -1,16 +1,38 @@
 export const ProjectsData = [
   {
     title: "Calculadora Básica",
-    description: "Calculadora aritmetica básica desarrollada con HTML, Tailwind CSS y Javascript",
+    description: "Calculadora de operaciones aritméticas básicas creada utilizando HTML, Tailwind CSS y JavaScript.",
     imageUrl: "https://i.ibb.co/rQK2KWr/anotherbasiccalculator.png",
     link: "https://anotherbasiccalculator.netlify.app/",
     repo: "https://github.com/M11gueeL/Calculator"
   },
   {
   	title: "Blog de noticias internacional",
-  	description: "Pequeño y básico blog de noticias internacionales desarrollado como practica de aprendizaje sobre el manejo de API's con React JS",
-	imageUrl: "https://i.postimg.cc/TwNcRNV6/newsblog.png",
-	link: "#",
-	repo: "https://github.com/M11gueeL/news-blog"  	
-  }
+  	description: "Blog sencillo y básico de noticias internacionales, desarrollado como proyecto de aprendizaje para practicar el manejo de API's con React JS.",
+  	imageUrl: "https://i.postimg.cc/TwNcRNV6/newsblog.png",
+  	link: "#",
+  	repo: "https://github.com/M11gueeL/news-blog"  	
+  },
+  {
+    title: "TicTacToe",
+    description: "Clásico juego Tic Tac Toe desarrollado con React Js",
+    imageUrl: "https://i.postimg.cc/Zn3RM2R5/Tic-Tac-Toe.png",
+    link: "https://66cd3d4799b66367331c6e62--whimsical-dieffenbachia-18f43b.netlify.app/",
+    repo: "https://github.com/M11gueeL/tictactoe"
+
+  },
+  {
+    title: "AudioYACK",
+    description: "Aplicación de Página Única (SPA) dedicada a una tienda de audífonos. Este sitio web es un excelente ejemplo de desarrollo front-end utilizando HTML y CSS, destacándose por su diseño responsivo que se adapta a diferentes tamaños de pantalla mediante CSS flexbox, grid y media queries.",
+    imageUrl: "https://i.postimg.cc/MGg3b1b7/Audio-YACK.png",
+    link: "https://stunning-alfajores-e2e3a4.netlify.app/",
+    repo: "https://github.com/M11gueeL/myWebSite"
+  },
+  {
+    title: "Weather App",
+    description: "Aplicación web del clima, desarrollada con React, Tailwind CSS y la API Weather, es una herramienta útil y práctica para cualquier persona que desee conocer las condiciones meteorológicas en cualquier parte del mundo. Su diseño intuitivo y su fácil uso la convierten en una solución ideal para usuarios de todos los niveles.",
+    imageUrl: "https://i.postimg.cc/x1hBtr9N/weather-App.png",
+    link: "https://66ccda6b310e990486bb97e1--grand-croissant-d91b39.netlify.app/",
+    repo: "https://github.com/M11gueeL/weatherApp"
+  },
 ];
